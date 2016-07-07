1dab66529f52dbd4fc19470435f85d6fa1f09694
Halima KOUNDI <hkoundi@gmail.com>
2016-07-07 16:24:53 +0100

    code smell data clumps

---
+++ DataClumps.Tests/CustomerTests.cs
@@ -0,0 +1,16 @@
+namespace DataClumps.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CustomerTests
+    {
+        [Test]
+        public void CustomerAddressSummaryIncludesHouseStreetCityPostCodeAndCountry()
+        {
+            var customer = new Customer("Dr", "Joseph", "Bloggs", "43", "Rankin Road", "London", "SW23 9YY", "United Kingdom");
+
+            Assert.AreEqual("43, Rankin Road, London, SW23 9YY, United Kingdom", customer.AddressSummary);
+        }
+    }
+}
